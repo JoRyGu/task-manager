1749a6135ea63aba4cd270eae71764c444e5d950
Josh Gude <gude.joshua@gmail.com>
2019-07-02 17:54:43 -0400

    Added Error Handling for User Creation

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,24 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/authCredentials.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+  async signUp(authCredentials: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentials;
 
+    const user = new User();
+    user.username = username;
+    user.password = password;
+    try {
+      await user.save();
+    } catch(err) {
+      if (err.code === '23505') {
+        throw new ConflictException('Username already exists.');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
 }
