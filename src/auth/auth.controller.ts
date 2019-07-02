cf50a1bb1025c5940d30ae945776137a69e36893
Josh Gude <gude.joshua@gmail.com>
2019-07-02 18:40:32 -0400

    Added GetUser Decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Inject, Body, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Inject, Body, ValidationPipe, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/authCredentials.dto';
 
